9fa8b9da8f64a66388299376f3910d5f8a9c3666
LIVIA HELOISA ALVES SOBRAL <03758569273@senacrs.edu.br>
2025-09-22 15:20:16 -0300

    AAAAAAAAAAAAAAAA

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,13 @@ import { Router } from 'express'
 import authRoutes from './auth.routes'
 import userRoutes from './user.routes'
 import countryRoutes from './country.routes'
-//import commentRoutes from './comment.routes'
+import commentRoutes from './comment.routes'
 
 const router = Router()
 
 router.use('/auth', authRoutes)
 router.use('/users', userRoutes)
 router.use('/country',countryRoutes)
-//router.use('/comment',commentRoutes)
+router.use('/comment',commentRoutes)
 
 export default router
