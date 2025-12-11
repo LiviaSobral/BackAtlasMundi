0fd8ddb37a4276b389b01f7e477d6865269438e4
LiviaSobral <liviaheloisa2005@gmail.com>
2025-12-11 14:53:34 -0300

    bbb

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -21,9 +21,9 @@ const controller = new CommentController()
      */
 
 router.post('/me', authMiddleware, validateDTO(CreateCommentDTO), controller.create.bind(controller)) // para criar commentario precisa do id do pais, a mensagem, um boolean se é uma pergunta ou n, e opcionalmente relatedTo (Q caso seja uma resposta a uma pergunta ele vai ter o id da pergunta)
-router.get('/country', controller.findByCountry.bind(controller)) //busca todos os commentarios por pais, recebe id do pais
-router.get('/user', controller.findByUser.bind(controller)) //busca todos os commentarios de um usuario em especifico, recebe id do usuario
-router.get('/id', controller.findById.bind(controller)) //busca um commentario por seu ID
+router.post('/country', controller.findByCountry.bind(controller)) //busca todos os commentarios por pais, recebe id do pais
+router.post('/user', controller.findByUser.bind(controller)) //busca todos os commentarios de um usuario em especifico, recebe id do usuario
+router.post('/id', controller.findById.bind(controller)) //busca um commentario por seu ID
 router.put('/me', authMiddleware, validateDTO(UpdateCommentDTO), controller.update.bind(controller)) // atualiza o commentario, recebendo todas as opções permitidas na criação do commentario
 router.delete('/me', authMiddleware, controller.remove.bind(controller)) //deleta Comentario, recebe id do comentario
 
